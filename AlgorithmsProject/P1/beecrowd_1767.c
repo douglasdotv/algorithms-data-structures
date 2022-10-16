54011d226e5500527cc365e532b017e940312a40
douglasdotv <douglasdotv@hotmail.com>
2022-10-16 18:29:27 -0300

    Add Algorithms Project P1

---
+++ AlgorithmsProject/P1/beecrowd_1767.c
@@ -0,0 +1,56 @@
+/*
+Beecrowd 1767 – Santa Claus Bag – https://www.beecrowd.com.br/judge/pt/problems/view/2534
+(No time limit restrictions)
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void imprime_qtd_brinquedos_e_peso(int[], int[], int, int);
+
+int main()
+{
+    int k = 0, n, pac, c = 50;
+    scanf("%d", &n);
+
+    do
+    {
+        scanf("%d", &pac);
+        int qt[pac], p[pac];
+        for (int i = 0; i < pac; i++)
+        {
+            scanf("%d %d", &(qt[i]), &(p[i]));
+        }
+        imprime_qtd_brinquedos_e_peso(p, qt, c, pac);
+        k++;
+    } while (k < n);
+
+    return 0;
+}
+
+void imprime_qtd_brinquedos_e_peso(int p[], int v[], int c, int n)
+{
+    int pos, valor, peso, max = 0, m = (int)pow(2, n), pesomax;
+    for (int i = 0; i < m; i++)
+    {
+        valor = peso = 0;
+        for (int j = 0; j < n; j++)
+        {
+            pos = (i >> j) % 2;
+            if (pos == 1)
+            {
+                valor += v[j];
+                peso += p[j];
+            }
+        }
+        if (peso <= c && valor > max)
+        {
+            max = valor;
+            pesomax = peso;
+        }
+    }
+    printf("%d brinquedos\n", max);
+    printf("Peso: %d kg", pesomax);
+    puts("\n");
+}
---
+++ AlgorithmsProject/P1/print_array_recursively.c
@@ -0,0 +1,41 @@
+/*
+Recursively printing an array, from the first element to the last one.
+*/
+
+#include <stdio.h>
+
+void imprimir_array_recursivamente(int[], int);
+void inserir_elementos(int[], int);
+
+int main()
+{
+    int n;
+    printf("Digite a quantidade n de elementos presentes no array: \n");
+    scanf("%d", &n);
+
+    int array[n];
+    inserir_elementos(array, n);
+
+    printf("Imprimindo os elementos do array, do primeiro ao último, recursivamente: \n");
+    imprimir_array_recursivamente(array, n);
+}
+
+void imprimir_array_recursivamente(int a[], int n)
+{
+    if (n == 1)
+    {
+        printf("%d ", a[0]);
+        return;
+    }
+    imprimir_array_recursivamente(a, n - 1);
+    printf("%d ", a[n - 1]);
+}
+
+void inserir_elementos(int a[], int n)
+{
+    printf("Insira os %d elementos do array: \n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+}
